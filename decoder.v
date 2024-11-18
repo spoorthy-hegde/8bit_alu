561e0ad1d80117985bdec21d62435778251bf57f
spoorthy-hegde <1rn22ec123.spoorthynagarajahegde@rnsit.ac.in>
2024-11-18 23:15:58 +0530

    Add files via upload

---
+++ decoder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/18/2024 11:14:54 PM
+// Design Name: 
+// Module Name: decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module decoder(
+
+    );
+endmodule
